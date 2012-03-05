fa8c8e36aa2c55a23a48659548625529fc6d461b
siva.chaitanya <siva.chaitanya@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2012-03-05 05:29:29 +0000

    Commented log messages to improve performance for dependent pick list

--- SVMXiPad-R4B1-7.7/Classes/SFMPage/BOTCustomControls/BotSpinner/BotSpinnerTextField.m
+++ SVMXiPad-R4B1-7.7/Classes/SFMPage/BOTCustomControls/BotSpinner/BotSpinnerTextField.m
@@ -135,17 +135,17 @@
    
     for(int j = 0 ; j< [TFHandler.spinnerData count];j++)
     {
-        NSLog(@"--");
+       // NSLog(@"--");
         NSString * obj = [TFHandler.validFor objectAtIndex:j];
-        NSLog(@"object :%@", obj);
+        //NSLog(@"object :%@", obj);
         obj = [obj stringByReplacingOccurrencesOfString:@" " withString:@""];
         if(obj == nil || [obj isEqualToString:@""])
         {
            // NSLog(@" object  %@" , obj);
             continue;
         }
-        NSLog(@" valid For %@" , TFHandler.validFor);
-        NSLog(@"spinner data %@ " , TFHandler.spinnerData);
+        //NSLog(@" valid For %@" , TFHandler.validFor);
+        //NSLog(@"spinner data %@ " , TFHandler.spinnerData);
         
         BitSet *bitObj = [[BitSet alloc] initWithData:obj];
         for (int k = 0; k < [bitObj size]; k++)
@@ -156,11 +156,11 @@
                 // for the controlling entry at index k
                 if(count == k)
                 {
-                    NSLog(@"Index of %@ = %d",obj,k);
+                   // NSLog(@"Index of %@ = %d",obj,k);
                     
                     //add to cityData
                     [spinner_Array addObject:[TFHandler.spinnerData objectAtIndex:j]];
-                    NSLog(@"SpinnerData %@", [TFHandler.spinnerData objectAtIndex:j]);
+                   // NSLog(@"SpinnerData %@", [TFHandler.spinnerData objectAtIndex:j]);
                     break;
                 }
             }
