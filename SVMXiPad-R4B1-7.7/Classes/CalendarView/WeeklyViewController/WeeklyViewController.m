c526a92e5c0be560aa428a28bc5207bff39b2ace
siva.chaitanya <siva.chaitanya@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2012-03-08 11:42:16 +0000

    Fix for Calendar Crash while switching from WeekView to DayView

--- SVMXiPad-R4B1-7.7/Classes/CalendarView/WeeklyViewController/WeeklyViewController.m
+++ SVMXiPad-R4B1-7.7/Classes/CalendarView/WeeklyViewController/WeeklyViewController.m
@@ -183,7 +183,9 @@
         event = [NSArray arrayWithObjects:subject, workOrderName, nil];
         
         weeksArray = [calendar getWeeksArray];
-        
+       if(currentSliderPositionIndex+1 > [weeksArray count])
+            currentSliderPositionIndex = 0;//Fix For Calendar Crash 
+            
         NSMutableArray * array = [weeksArray objectAtIndex:currentSliderPositionIndex];
         
         NSUInteger dayIndex = 0;
