dd1601b565ed2ab6bddeac1f53b864c862acc748
nagendra.v <nagendra.v@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2012-05-18 12:39:34 +0000

    Fix for defect #4081(Progress Bar)

--- SVMXiPadMobile8.3/Classes/HomeScreenViewController/HomeScreen/iPadScrollerViewController.h
+++ SVMXiPadMobile8.3/Classes/HomeScreenViewController/HomeScreen/iPadScrollerViewController.h
@@ -10,17 +10,21 @@
 #import "BSPreviewScrollView.h"
 #import "TapImage.h"
 #import "LocalizationGlobals.h"
-
+#import "WSInterface.h"
 @class CalendarController;
 @class iServiceAppDelegate;
 
 @interface iPadScrollerViewController : UIViewController
-<BSPreviewScrollViewDelegate, TapImageDelegate>
+<BSPreviewScrollViewDelegate, TapImageDelegate,RefreshProgressBar>
 {
 	NSMutableArray * scrollPages;
+    IBOutlet UIView *ProgressView;
+    IBOutlet UIView * transparent_layer;
+    IBOutlet UILabel *display_pecentage;
 
     IBOutlet BSPreviewScrollView * scrollViewPreview;
     
+    IBOutlet UILabel *progressTitle;
     //Abinash
     IBOutlet UIActivityIndicatorView * activity;
 
@@ -36,8 +40,28 @@
     iServiceAppDelegate * appDelegate;
     
     BOOL isInternetAvailable;
+    NSTimer * initial_sync_timer;
+    float total_progress;
+    BOOL Sync_status;
+    int Total_calls,current_num_of_call;
+    NSInteger temp_percentage;
+    
+    UIAlertView * internet_alertView;
 }
-
+@property (nonatomic , retain) UIAlertView * internet_alertView;
+@property (nonatomic) NSInteger temp_percentage;
+@property (nonatomic , retain) IBOutlet UILabel *display_pecentage;
+@property (nonatomic)  int Total_calls;
+@property (nonatomic) int current_num_of_call;
+@property (nonatomic , retain) IBOutlet UIView * transparent_layer;
+@property (nonatomic)  BOOL Sync_status;
+@property (retain, nonatomic) IBOutlet UILabel *description_label;
+@property (retain, nonatomic) IBOutlet UILabel *download_desc_label;
+@property (retain, nonatomic) IBOutlet UILabel *StepLabel;
+@property (nonatomic)  float total_progress;
+@property (nonatomic , retain) NSTimer * initial_sync_timer;
+@property (retain, nonatomic) IBOutlet UIProgressView *progressBar;
+@property (nonatomic , retain)  IBOutlet UILabel *progressTitle;
 @property (nonatomic, retain) NSArray * scrollPages;
 
 
@@ -57,7 +81,22 @@
 //Abinash
 -(void)logout;
 -(void)sync;
+- (void) doMetaAndDataSync;
+-(void)disableControls;
+-(void)enableControls;
+-(void)InitsyncSetting;
+-(void)initialDataSetUpAfterSyncOrLogin;
 
 
-
+//sahana
+-(void)fillNumberOfStepsCompletedLabel;
+-(void)showAlertForInternetUnAvailability;
+-(void)doMetaSync;
+-(void)doDataSync;
+-(void)doTxFetch;
+-(void)doAfterSyncSetttings;
+-(void)showAlertViewForAppwasinBackground;
+- (void)continueMetaAndDataSync;
+-(void)RefreshProgressBarNativeMethod:(NSString *)sync;
+#define NUM_CALLS_INTIAL_SYNC  15;
 @end
