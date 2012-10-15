ceaa9af7e0ebcb579509b2fa5cfed0a21fd38479
siva.chaitanya <siva.chaitanya@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2012-10-15 07:29:08 +0000

    Renamed 9.4 to 9.3.1

---
+++ SVMXiPadMobile9.3.1/Classes/SFMPage/DetailViewController/TimerClass/TimerClass.h
@@ -0,0 +1,44 @@
+//
+//  TimerClass.h
+//  MultipleDetailViews
+//
+//  Created by Samman Banerjee on 27/08/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum
+{
+    TimerClassTypeResolution, 
+    TimerClassTypeRestoration
+}TimerClassType;
+
+@interface TimerClass : UIViewController
+{
+    NSTimer * timer;
+    NSUInteger days, hours, minutes, seconds;
+    
+    IBOutlet UILabel * dayLabel, * hourLabel, * minuteLabel, * secondLabel;
+    IBOutlet UILabel * flasher1, * flasher2;
+    
+    NSMutableDictionary * slaTimer;
+    
+    TimerClassType type;
+    
+    BOOL isTimerRunning;
+}
+
+@property TimerClassType type;
+@property (nonatomic, retain) NSMutableDictionary * slaTimer;
+
+- (void) StartCountdownFromDays:(NSUInteger)_days Hours:(NSUInteger)_hours Minutes:(NSUInteger)_minutes Seconds:(NSUInteger)_seconds;
+- (void) ResetTimer;
+
+- (void) DecrSec;
+- (void) DecrMin;
+- (void) DecrHrs;
+- (void) DecrDay;
+- (void) updateTimerLabel:(NSString *) updatedTime;
+
+@end
