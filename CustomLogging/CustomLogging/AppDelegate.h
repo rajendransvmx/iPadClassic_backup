782ff4c97b32c036dcd612ca1d402df76800d68a
siva.chaitanya <siva.chaitanya@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2012-12-14 09:26:29 +0000

    Importing CustomLogging

---
+++ CustomLogging/CustomLogging/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CustomLogging
+//
+//  Created by Siva Manne on 15/05/12.
+//  Copyright (c) 2012 ServiceMax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CustomLogging/CustomLogging/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  CustomLogging
+//
+//  Created by Siva Manne on 15/05/12.
+//  Copyright (c) 2012 ServiceMax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)logError:(id)sender;
+- (IBAction)logWarn:(id)sender;
+- (IBAction)logInfo:(id)sender;
+- (IBAction)logDebug:(id)sender;
+@end
