cc1c906836fc06cd169f7b6f223dba08bff27d30
siva.chaitanya <siva.chaitanya@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2013-01-07 08:03:56 +0000

    Initial check-in of ServiceMax Mobile v 10.3 branch.
    Removed unused help files and folders
    1.       action.jpg
    2.       back-o.png
    3.       calendar-day.png
    4.       calendar-week.png
    5.       chatter-button.png
    6.       home-page-o.png
    7.       iService-help1_with-slices.gif
    8.       iService-help1_with-slices_09.gif
    9.       iService-help1_with-slices_11.gif
    10.   iService-help1_with-slices_16.gif
    11.   iService-help1_with-slices_18.gif
    12.   iService-help1_with-slices_19.gif
    13.   iService-help1_with-slices_21.gif
    14.   iService-help1_with-slices_22.gif
    15.   iService-help1_with-slices_23.gif
    16.   iService-help1_with-slices_27.gif
    17.   iService-help1_with-slices_28.gif
    18.   iService-help1_with-slices_40.gif
    19.   iService-help1_with-slices_41.gif
    20.   iService-help1_with-slices_42.gif
    21.   iService-help1_with-slices_43.gif
    22.   iService-help1_with-slices_44.gif
    23.   iService-help1_with-slices_46.gif
    24.   iService-help1_with-slices_47.gif
    25.   iService-help1_with-slices_48.gif
    26.   iService-help1_with-slices_49.gif
    27.   iService-help1_with-slices_54.gif
    28.   iService-help1_with-slices_55.gif
    29.   iService-help1_with-slices_56.gif
    30.   iService-help1_with-slices_59.gif
    31.   lookup-o.png
    32.   map-view-o.png
    33.   mobile.jpg
    34.   Multi add-o.jpg
    35.   multi-add-o-1.png
    36.   online.jpg
    37.   product-manual-button.png
    38.   refresh-button.png
    39.   service-report-o.png
    40.   signature-o-1.jpg
    41.   status.jpg
    42.   switchview.jpg
    43.   sync-yellow.jpg
    44.   synchronize.jpg
    45.   Thumbs.db
    46.   untitled.bmp
    47.   view-record-o.png

---
+++ SVMXiPadMobile10.3/Classes/Chatter/ImageCache/ImageCacheClass.m
@@ -0,0 +1,83 @@
+//
+//  MyImageClass.m
+//  MiniDirectory
+//
+//  Created by Samman Banerjee on 30/09/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "ImageCacheClass.h"
+
+
+@implementation ImageCacheClass
+
+@synthesize imageCache;
+
+- (id) init
+{
+    self = [super init];
+	if (self)
+	{
+		imageCache = [[NSMutableDictionary alloc] initWithCapacity:0];
+	}
+	return self;
+}
+
+- (void) clearMemory
+{
+	[imageCache removeAllObjects];
+}
+
+- (UIImage *) getImage:(NSString *)filename
+{
+	if (filename == nil)
+		return nil;
+    
+    NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString * documentsDirectoryPath = [paths objectAtIndex:0];
+    filename = [documentsDirectoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@", filename]];
+
+	UIImage * image = [imageCache objectForKey:filename];
+
+	if (nil == image)
+	{
+		if ([imageCache count] > 5)
+		{
+			[imageCache removeAllObjects];
+		}
+		// NSString * imageFile = [NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle] resourcePath], filename];
+		if ([filename isEqualToString:@""])
+			return nil;
+		image = [UIImage imageWithContentsOfFile:filename];
+		if (nil != image)
+			[imageCache setObject:image forKey:filename];
+		else
+			return nil;
+	}
+
+	return image;
+}
+
+-(void) dealloc
+{
+	[imageCache removeAllObjects];
+	[imageCache release];
+	[super dealloc];
+}
+
+@end
+
+/*
+@implementation UIImage (ImageLoadingExtension)
+
++ (UIImage *) newImageFromResource:(NSString *) filename
+{
+	NSString * imageFile = [[NSString alloc] initWithFormat:@"%@/%@", [[NSBundle mainBundle] resourcePath], filename];
+    UIImage * image = nil;
+    image = [[UIImage alloc] initWithContentsOfFile:imageFile];
+    [imageFile release];
+    return image;
+}
+
+@end
+*/
