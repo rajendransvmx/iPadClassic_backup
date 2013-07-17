d3343d8c916a058893ab3cd855dff79c45e633bd
praveen.r <praveen.r@bitordertech.com>
2013-07-17 12:44:00 +0000

    Adding new files PlistUtility.h and PlistUtility.m

---
+++ SVMXiPadMobile12.0/Classes/SFMPage/PlistUtility/plistUtility.h
@@ -0,0 +1,18 @@
+//
+//  plistUtility.h
+//  iService
+//
+//  Created by Sahana BM on 16/07/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface plistUtility : NSObject
++(void)writeIntoPlist:(NSString *)plistName data:(NSMutableDictionary *)dict;
++(NSMutableDictionary *)readFromPlist:(NSString *)plistName;
++(void)deletePlist:(NSString*)plistName;
++(NSString *)getFilePath:(NSString *)fileName;
++(BOOL)DoesFileExist:(NSString *)fileName;
++(void)clearPlist:(NSString *)clearPlist;
+@end
