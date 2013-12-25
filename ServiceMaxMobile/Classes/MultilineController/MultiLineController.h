6c107dbac4104fc0d06d4335929d3c20e57291b7
guru.prasad <gurusvmx@users.noreply.github.com>
2013-12-25 08:47:17 +0000

    13.0.0.4

---
+++ ServiceMaxMobile/Classes/MultilineController/MultiLineController.h
@@ -0,0 +1,20 @@
+//
+//  MultiLineController.h
+//  ManualDataSyncUI
+//
+//  Created by Parashuram on 11/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AppDelegate.h"
+
+@interface MultiLineController : UISegmentedControl
+{
+    BOOL initialized;
+
+}
+
+//- (void)setSubTitle:(NSString *)title forSegmentAtIndex:(NSUInteger)segment;//  Unused methods
+
+@end
