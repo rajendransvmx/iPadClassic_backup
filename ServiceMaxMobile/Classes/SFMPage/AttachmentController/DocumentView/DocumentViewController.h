a5c7ecece6c4251875c2a59bcbdf6e847499ebbf
guru.prasad <gurusvmx@users.noreply.github.com>
2014-02-10 07:54:00 +0000

    14.10.001

---
+++ ServiceMaxMobile/Classes/SFMPage/AttachmentController/DocumentView/DocumentViewController.h
@@ -0,0 +1,46 @@
+//
+//  DocumentViewController.h
+//  ServiceMaxMobile
+//
+//  Created by Kirti on 07/11/13.
+//  Copyright (c) 2013 SivaManne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DocumentViewCell.h"
+#import "AttachmentViewController.h"
+
+@protocol DocumentViewControllerDelegate <NSObject>
+
+-(void) displayAttachment:(NSString *)attachmentId fielName:(NSString *)fileName category:(NSString *)categoty;
+
+@end
+
+
+@interface DocumentViewController : AttachmentViewController<UITableViewDataSource,UITableViewDelegate> {
+    IBOutlet UITableView *mainTableView;
+    BOOL   isInEditMode;
+    NSMutableDictionary *selectedIdDictionary;
+    BOOL   isViewProcess;
+    NSString *tapToDownloadString;
+}
+@property (nonatomic,retain)UITableView *mainTableView;
+@property (nonatomic,assign)BOOL   isInEditMode;
+@property (nonatomic, retain) NSMutableArray *documentsArray;
+@property (nonatomic,retain)NSMutableDictionary *selectedIdDictionary;
+@property (nonatomic,assign) BOOL   isViewProcess;
+@property (nonatomic,retain)IBOutlet UIButton *editButton;
+@property (nonatomic, retain)IBOutlet UIButton *cancelButton;
+@property (nonatomic, assign) id<DocumentViewControllerDelegate> delegate;
+@property (nonatomic, retain) IBOutlet UIImageView *pencilIcon;
+@property (nonatomic,retain)NSString *tapToDownloadString;
+@property (nonatomic, retain) IBOutlet UILabel *titleLabel;
+
+- (IBAction)updateDocumentList:(id)sender;
+- (IBAction)cancelButtonTapped:(id)sender;
+- (void) refreshDocuments;
+- (void) deleteAttachment:(NSString *)localId;
+-(void)handleErrorForCell:(DocumentViewCell *)cell
+           withAttachment:(NSDictionary *)documentDict
+                andStatus:(int)status; //9212
+@end
