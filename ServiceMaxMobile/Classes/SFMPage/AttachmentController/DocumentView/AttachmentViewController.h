d46e3ef57939900b6a99bddf7dde1a178c14302c
guru.prasad <gurusvmx@users.noreply.github.com>
2014-04-30 07:25:24 +0000

    Spring 14 iPad Tag : 14.29.104

---
+++ ServiceMaxMobile/Classes/SFMPage/AttachmentController/DocumentView/AttachmentViewController.h
@@ -0,0 +1,50 @@
+//
+//  AttachmentViewController.h
+//  ServiceMaxMobile
+//
+//  Created by Shravya shridhar on 11/17/13.
+//  Copyright (c) 2013 SivaManne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define DOWNLOAD_CANCEL_ALERT   57361
+
+
+@interface AttachmentViewController : UIViewController {
+    
+    NSMutableDictionary *attachmentProgressBarsDictionary;
+      NSDictionary *selectAttachmentForCancel;
+}
+
+@property(nonatomic,retain) NSMutableDictionary *attachmentProgressBarsDictionary;
+@property(atomic,retain) NSDictionary *selectAttachmentForCancel;
+
+
+- (void)addAttachmentDownloadObserver;
+- (void)removeAttachmentDownloadObserver;
+
+- (void)addProgressBar:(UIProgressView *)progressBar ForId:(NSString *)attachmentId;
+- (void)updateProgressBarWithValue:(CGFloat)value forId:(NSString *)attachmentId;
+- (void)removeProgressbarForId:(NSString *)attachmentId;
+
+- (void)updateStatusProgress:(CGFloat)progress forId:(NSString *)attachmentId;
+- (void)downloadCompleteForId:(NSString *)attachmentId ;
+- (void)downloadFailedForId:(NSString *)attachmentId withError:(NSError *)error;
+- (void)showInternetnotAvailableAlert:(NSString *)title ;
+- (void)cleanUpBeforeUnload;
+- (void)downloadAttachment:(NSDictionary *)attachment;
+- (float)getProgress:(NSString *)progressString;
+
+- (void)addDataSyncObserver;
+- (void)removeDataSyncObserver;
+- (UIProgressView *)createProgressBar;
+/*- (void)handleError:(NSError *)error forAttachmentId:(NSString *)attachmentId;*/
+
+- (void)reloadViewData;
+- (void)cancelDownloadForAttachment:(NSDictionary *)attachmentDict;
+
+- (void)showAlertForCancelConfirmationAlert:(NSString *)title
+                                 andMessage:(NSString *)message;
+
+
+@end
