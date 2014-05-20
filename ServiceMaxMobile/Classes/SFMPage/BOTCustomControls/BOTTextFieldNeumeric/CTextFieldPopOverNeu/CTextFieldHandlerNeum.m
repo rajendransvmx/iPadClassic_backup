f8174ffb7094bc8eaa772dc9c18f2a97b7911901
guru.prasad <gurusvmx@users.noreply.github.com>
2014-05-20 14:48:57 +0000

    Tag for iPad Spring 14 SP 14.29.110

---
+++ ServiceMaxMobile/Classes/SFMPage/BOTCustomControls/BOTTextFieldNeumeric/CTextFieldPopOverNeu/CTextFieldHandlerNeum.m
@@ -0,0 +1,349 @@
+//
+//  CTextFieldHandlerNeum.m
+//  CustomClassesipad
+//
+//  Created by Developer on 4/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CTextFieldHandlerNeum.h"
+#import "CTextField.h"
+#import "Utility.h"
+
+@implementation CTextFieldHandlerNum
+
+@synthesize delegate;
+@synthesize POC;
+@synthesize rect;
+@synthesize PopOverView;
+@synthesize lableValue;
+@synthesize control_type;
+@synthesize percent_count;
+@synthesize countflag;
+
+-(id)init
+{
+    self.percent_count = 0;
+    return self;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSScanner * scanner = [NSScanner scannerWithString:string];
+    int val;
+    //7int Dotcount =0;
+    BOOL isInt = [scanner scanInt:&val] && [scanner isAtEnd];
+    if([self.control_type isEqualToString:@"percent"])
+    {
+        BOOL isNegative = FALSE; //10346
+        
+        if([string isEqualToString: @"."])
+        {
+            NSString * substring = [textField.text substringFromIndex:0];
+            NSRange range = [substring rangeOfString:@"."];
+            if(range.location == NSNotFound)
+            {
+                countflag = TRUE;
+                //samman sir code begins
+                NSString * text = textField.text;
+                if ([string length] != 0)
+                    text = [NSString stringWithFormat:@"%@%@", text, string];
+                else
+                    text = [text substringToIndex:[text length]-1];
+                [delegate didChangeText:text];
+                //code ends
+                return TRUE;
+            }
+            
+            return FALSE;
+        }
+        //10346
+        else if ([string isEqualToString:@"-"] && range.location == 0)
+        {
+            isNegative = TRUE;
+        }
+        
+        if (!isInt && (![string length] == 0) && !isNegative)
+            return  NO;
+    
+        if(countflag == TRUE)
+        {
+            NSString * substring = [textField.text substringFromIndex:0];
+            NSRange range = [substring rangeOfString:@"."];
+            NSInteger textlength = [textField.text length];
+            NSUInteger rangeLength = range.location;
+            NSInteger forceLength = textlength - rangeLength;
+          
+            if(forceLength > 2)
+            {
+                if([string length] == 0)
+                {
+                    //samman sir code begins
+                    NSString * text = textField.text;
+                    if ([string length] != 0)
+                        text = [NSString stringWithFormat:@"%@%@", text, string];
+                    else
+                        text = [text substringToIndex:[text length]-1];
+                    [delegate didChangeText:text];
+                    //code ends
+                    return YES;
+                }
+                return NO;
+            }
+           
+        }
+        //samman sir code begins
+        NSString * text = textField.text;
+        if ([string length] != 0)
+            text = [NSString stringWithFormat:@"%@%@", text, string];
+        else
+            text = [text substringToIndex:[text length]-1];
+        [delegate didChangeText:text];
+        //code ends
+        return TRUE;
+    }
+    if([self.control_type isEqualToString:@"currency"])
+    {
+        BOOL isNegative = FALSE; //10346
+        
+        if([string isEqualToString: @"."])
+        {
+            NSString * substring = [textField.text substringFromIndex:0];
+            NSRange range = [substring rangeOfString:@"."];
+            if(range.location == NSNotFound)
+            {
+//                countflag = TRUE;
+                //samman sir code begins
+                NSString * text = textField.text;
+                if ([string length] != 0)
+                    text = [NSString stringWithFormat:@"%@%@", text, string];
+                else
+                    text = [text substringToIndex:[text length]-1];
+                [delegate didChangeText:text];
+                //code ends
+                return TRUE;
+            }
+            
+            return FALSE;
+        }
+        //10346
+        else if ([string isEqualToString:@"-"] && range.location == 0)
+        {
+            isNegative = TRUE;
+        }
+        
+        if (!isInt && (![string length] == 0) && !isNegative)
+            return  NO;
+      
+        //10346
+        if([Utility containsString:@"." inString:textField.text])
+        {
+            countflag = TRUE;
+        }
+        else
+        {
+            countflag = FALSE;
+        }
+        
+        //10346
+        if (!isNegative)
+        {
+            NSUInteger textlength = 0;
+            if(countflag == TRUE)
+            {
+                if ([textField.text hasPrefix:@"-"])
+                {
+                    textlength = textField.text.length - 1;
+                }
+                else
+                {
+                    textlength = textField.text.length;
+                }
+                if (textlength > precisionValue)
+                {
+                    if([string length] == 0)
+                    {
+                        //samman sir code begins
+                        NSString * text = textField.text;
+                        if ([string length] != 0)
+                            text = [NSString stringWithFormat:@"%@%@", text, string];
+                        else
+                            text = [text substringToIndex:[text length]-1];
+                        [delegate didChangeText:text];
+                        //code ends
+                        return YES;
+                    }
+                    return NO;
+                }
+            }
+            else
+            {
+                if ([textField.text hasPrefix:@"-"])
+                {
+                    textlength = textField.text.length - 1;
+                    if (textlength == length && [string length] > 0)
+                    {
+                        return NO;
+                    }
+                }
+                else
+                {
+                    textlength = textField.text.length + string.length;
+                }
+                
+                if (textlength > length)
+                {
+                    if([string length] == 0)
+                    {
+                        //samman sir code begins
+                        NSString * text = textField.text;
+                        if ([string length] != 0)
+                            text = [NSString stringWithFormat:@"%@%@", text, string];
+                        else
+                            text = [text substringToIndex:[text length]-1];
+                        [delegate didChangeText:text];
+                        //code ends
+                        return YES;
+                    }
+                    return NO;
+                }
+            }
+        }
+        //samman sir code begins
+        NSString * text = textField.text;
+        if ([string length] != 0)
+            text = [NSString stringWithFormat:@"%@%@", text, string];
+        else
+            text = [text substringToIndex:[text length]-1];
+        [delegate didChangeText:text];
+        //code ends
+        return TRUE;
+    }
+    if([self.control_type isEqualToString:@"double"])
+    {
+        BOOL isNegative = FALSE; //10346
+        
+        if([string isEqualToString: @"."])
+        {
+            NSString * substring = [textField.text substringFromIndex:0];
+            NSRange range = [substring rangeOfString:@"."];
+            if(range.location == NSNotFound)
+            {
+                countflag = TRUE;
+                //samman sir code begins
+                NSString * text = textField.text;
+                if ([string length] != 0)
+                    text = [NSString stringWithFormat:@"%@%@", text, string];
+                else
+                    text = [text substringToIndex:[text length]-1];
+                [delegate didChangeText:text];
+                //code ends
+                return TRUE;
+            }
+            
+            return FALSE;
+        }
+        //10346
+        else if ([string isEqualToString:@"-"] && range.location == 0)
+        {
+            isNegative = TRUE;
+        }
+        
+        if (!isInt && (![string length] == 0) && !isNegative)
+            return  NO;
+        
+        if(countflag == TRUE)
+        {
+            NSString * substring = [textField.text substringFromIndex:0];
+            NSRange range = [substring rangeOfString:@"."];
+            NSInteger textlength = [textField.text length];
+            NSUInteger rangeLength = range.location;
+            NSInteger forceLength = 0;
+            if (range.length > 0)
+                forceLength = textlength - rangeLength;
+            
+            if(forceLength > 2)
+            {
+                if([string length] == 0)
+                {
+                    //samman sir code begins
+                    NSString * text = textField.text;
+                    if ([string length] != 0)
+                        text = [NSString stringWithFormat:@"%@%@", text, string];
+                    else
+                        text = [text substringToIndex:[text length]-1];
+                    [delegate didChangeText:text];
+                    //code ends
+                    return YES;
+                }
+                return NO;
+            }
+            
+        }
+        NSString * text = textField.text;
+        if ([string length] != 0)
+            text = [NSString stringWithFormat:@"%@%@", text, string];
+        else
+            text = [text substringToIndex:[text length]-1];
+        [delegate didChangeText:text];
+
+        [delegate didChangeText:text];
+        return TRUE;
+
+    }
+    if([self.control_type isEqualToString:@"phone"])
+    {
+        if (!isInt && (![string length] == 0))
+            return  NO;
+        //samman sir code begins
+        NSString * text = textField.text;
+        if ([string length] != 0)
+            text = [NSString stringWithFormat:@"%@%@", text, string];
+        else
+            text = [text substringToIndex:[text length]-1];
+        [delegate didChangeText:text];
+        //code ends
+        return  TRUE;
+
+    }
+    if (!isInt && (![string length] == 0))
+        return  NO;
+    //samman sir code begins
+    NSString * text = textField.text;
+    if ([string length] != 0)
+        text = [NSString stringWithFormat:@"%@%@", text, string];
+    else
+        text = [text substringToIndex:[text length]-1];
+    [delegate didChangeText:text];
+    //code ends
+    return  TRUE;
+
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{    
+    CTextField * parent = delegate;
+    precisionValue = parent.precision; //10346
+    length = parent.length;
+    countflag = FALSE;
+
+    [parent.controlDelegate controlIndexPath:parent.indexPath];
+    return YES;
+}
+
+/*
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;   // return NO to not change text
+{
+    NSString * text = textField.text;
+    if ([string length] != 0)
+        text = [NSString stringWithFormat:@"%@%@", text, string];
+    else
+        text = [text substringToIndex:[text length]-1];
+
+    [delegate didChangeText:text];
+    return YES;
+}
+*/
+
+
+@end
