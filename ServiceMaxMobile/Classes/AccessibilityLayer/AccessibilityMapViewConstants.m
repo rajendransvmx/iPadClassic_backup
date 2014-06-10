3a8b82acd839eb5e1dcb9630280dc1d3e947fd99
guru.prasad <gurusvmx@users.noreply.github.com>
2014-06-10 11:44:14 +0000

    Tag for iPad Spring 14 SP 14.29.119

---
+++ ServiceMaxMobile/Classes/AccessibilityLayer/AccessibilityMapViewConstants.m
@@ -0,0 +1,35 @@
+//
+//  AccessibilityMapViewConstants.m
+//  ServiceMaxMobile
+//
+//  Created by Radha Sathyamurthy on 19/04/14.
+//  Copyright (c) 2014 SivaManne. All rights reserved.
+//
+
+#import "AccessibilityMapViewConstants.h"
+
+NSString * const kAccWoNumber                   = @"Label_WoNumber";
+NSString * const kAccWoDescription              = @"TextView_WoDescription";
+NSString * const kAccContactName                = @"Label_ContactName";
+NSString * const kAccContactAddress             = @"TextView_ContactAddress";
+NSString * const kAccContactEmail               = @"Label_ContactEmail";
+NSString * const kAccProblemDescription         = @"TextView_ProblemDescription";
+NSString * const kAccProblemCode                = @"Label_ProblemCode";
+
+//Annotations identifier
+NSString * const kAccHomeAnnotationPin          = @"MKPinAnnotation_Technician";
+NSString * const kAccWOAnnotationPin            = @"MKPinAnnotation_WorkOrder";
+
+//Value
+NSString * const kAccHomePinColor               = @"MKPinAnnotation_PinRed";
+NSString * const kAccWOPinColor                 = @"MKPinAnnotation_PinPurple";
+
+//Location popover identifier
+NSString * const kAccJobDetails                 = @"Button_JobDetails";
+NSString * const kAccWODrivingDirection         = @"Button_JobDirections";
+
+NSString * const kAccHomeDrivingDirections      = @"Button_HomeDirections";
+
+@implementation AccessibilityMapViewConstants
+
+@end
