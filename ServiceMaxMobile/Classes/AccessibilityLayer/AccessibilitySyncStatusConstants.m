a97cd62e08278d45bbd55e757311026a1938b8a8
guru.prasad <gurusvmx@users.noreply.github.com>
2014-07-04 11:14:58 +0000

    Tag for iPad Sum 14 14.39.104

---
+++ ServiceMaxMobile/Classes/AccessibilityLayer/AccessibilitySyncStatusConstants.m
@@ -0,0 +1,37 @@
+//
+//  AccessibilitySyncStatusConstants.m
+//  ServiceMaxMobile
+//
+//  Created by Radha Sathyamurthy on 11/04/14.
+//  Copyright (c) 2014 SivaManne. All rights reserved.
+//
+
+#import "AccessibilitySyncStatusConstants.h"
+
+NSString * const kAccLastDataSyncTimeLabel          = @"Label_LastDatSyncLabel";
+NSString * const kAccNextDataSyncTimeLabel          = @"Label_NextDataSyncLabel";
+NSString * const kAccLastDataSyncStatusLabel        = @"Label_LastDataSyncStatusLabel";
+NSString * const kAccLastDataSync                   = @"Label_LastDatSync";
+NSString * const kAccNextDataSync                   = @"Label_NextDataSync";
+NSString * const kAccLastDataSyncStatus             = @"Label_LastDataSyncStatus";
+
+NSString * const kAccPushLogStatusLabel             = @"Label_PushLogStatusLabel";
+NSString * const kAccPushLogStatus                  = @"Label_PushLogStatus";
+
+NSString * const kAccLastConfigSyncTimeLabel        = @"Label_LastConfigSyncLabel";
+NSString * const kAccNextConfigSyncTimeLabel        = @"Label_NextConfigSyncLabel";
+NSString * const kAccLastConfigSyncStatusLabel      = @"Label_LastConfigSyncStatusLabel";
+NSString * const kAccLastConfigSync                 = @"Label_LastConfigSync";
+NSString * const kAccNextConfigSync                 = @"Label_NextConfigSync";
+NSString * const kAccLastConfigSyncStatus           = @"Label_LastConfigSyncStatus";
+
+NSString * const kAccLastDataPurgeTimeLabel         = @"Label_LastDataPurgeLabel";
+NSString * const kAccNextDataPurgeTimeLabel         = @"Label_NextDataPurgeLabel";
+NSString * const kAccLastDataPurgeStatusLabel       = @"Label_LastDataPurgeStatusLabel";
+NSString * const kAccLastDataPurge                  = @"Label_LastDataPurge";
+NSString * const kAccNextDataPurge                  = @"Label_NextDataPurge";
+NSString * const kAccLastDataPurgeStatus            = @"Label_LastDataPurgeStatus";
+
+@implementation AccessibilitySyncStatusConstants
+
+@end
