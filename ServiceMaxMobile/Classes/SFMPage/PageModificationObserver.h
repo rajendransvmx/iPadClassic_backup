12edf488e83470605722eb18932ffa45a00f14f7
guru.prasad <gurusvmx@users.noreply.github.com>
2014-07-22 06:09:46 +0000

    Tag for iPad Sum 14 14.40.001

---
+++ ServiceMaxMobile/Classes/SFMPage/PageModificationObserver.h
@@ -0,0 +1,30 @@
+//
+//  PageModificationObserver.h
+//  ServiceMaxMobile
+//
+//  Created by Vipindas on 6/6/14.
+//  Copyright (c) 2014 ServiceMax, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PageModificationObserver : NSObject
+
+- (id)initWithModifiedDataDictionary:(NSMutableDictionary *)dataDictionary
+                            recordId:(NSString *)recordId
+                  andObjectOrApiName:(NSString *)apiName;
+
+- (id)initWithModifiedEventStartDate:(NSString *)startDate
+                             endDate:(NSString *)endDate
+                            recordId:(NSString *)recordId
+                             andSfId:(NSString *)salesForceId;
+
+- (NSString *)jsonString;
+
+- (void)doDataComparison;
+
+- (BOOL)hasDataModified;
+- (BOOL)updateModifiedFieldsData;
+
+@end
