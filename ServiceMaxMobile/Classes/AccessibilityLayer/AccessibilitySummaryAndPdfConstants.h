4c0c8d3183c4714014ebc90da8f0ca38694e2d97
guru.prasad <gurusvmx@users.noreply.github.com>
2014-09-19 11:33:48 +0000

    Tag for iPad Sum 14 14.40.007

---
+++ ServiceMaxMobile/Classes/AccessibilityLayer/AccessibilitySummaryAndPdfConstants.h
@@ -0,0 +1,22 @@
+//
+//  AccessibilitySummaryAndPdfConstants.h
+//  ServiceMaxMobile
+//
+//  Created by Radha Sathyamurthy on 19/04/14.
+//  Copyright (c) 2014 SivaManne. All rights reserved.
+//
+
+#import "AccessibilityGlobalConstants.h"
+
+/*Summary*/
+NSString * const kAccPdf;
+NSString * const kAccSummarySignature;
+NSString * const kAccTotalAmount;
+
+/*Pdf*/
+NSString * const kAccEmail;
+NSString * const kAccPdfWebview;
+
+@interface AccessibilitySummaryAndPdfConstants : AccessibilityGlobalConstants
+
+@end
---
+++ ServiceMaxMobile/Classes/WSInterface/WSResponseParser/WSResponseParser.h
@@ -0,0 +1,27 @@
+//
+//  WSResponseParser.h
+//  iService
+//
+//  Created by Siva Manne on 02/01/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "DataBaseGlobals.h"
+#import "SBJsonParser.h"
+void SMXLog(int level,const char *methodContext,int lineNumber,NSString *message);
+@class DataBase;
+@class databaseIntefaceSfm;
+@interface WSResponseParser : NSObject
+@property (nonatomic, assign) DataBase * dataBase;
+@property (nonatomic, assign) databaseIntefaceSfm * dataBaseInterface;
++ (id) classForEventName:(NSString *)eventName eventType:(NSString *)eventType;
+- (BOOL) parseResponse:(NSArray *)result;
+- (void)insertRecords:(NSArray *)array intoTable:(NSString *)tableName;
+- (NSString *)createInsertSQLStatmentWithDict:(NSDictionary *)dict forTable:(NSString *)tableName;
+- (void) execStatementOnDataBase:(NSString *)queryStatement;
+- (void) createTable:(NSString *) tableName;
+- (NSString *) getUUID;
+- (id) getRequiredData:(NSString *)key;
+- (BOOL) responseTypeDCForLastIndexTwo; //Get price performance
+@end
