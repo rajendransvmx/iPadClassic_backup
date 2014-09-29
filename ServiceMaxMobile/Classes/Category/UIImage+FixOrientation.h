8b2a6871682ebf788919707d9ac180c8785b101f
guru.prasad <gurusvmx@users.noreply.github.com>
2014-09-29 13:41:21 +0000

    Tag for iPad Sum 14 14.50.004

---
+++ ServiceMaxMobile/Classes/Category/UIImage+FixOrientation.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+fixOrientation.h
+//  ServiceMaxMobile
+//
+//  Created by Shubha S on 16/07/14.
+//  Copyright (c) 2014 Servicemax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (FixOrientation)
+
+- (UIImage *)fixOrientation;
+
+@end
---
+++ ServiceMaxMobile/Classes/TroubleShooting/Troubleshooting.h
@@ -0,0 +1,132 @@
+//
+//  Troubleshooting.h
+//  iService
+//
+//  Created by Samman Banerjee on 27/08/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "iOSInterfaceObject.h"
+#import "ProductManual.h"
+#import "Chatter.h"
+#import "ZipArchive.h"
+#import "AppDelegate.h"
+
+@protocol TroubleshootingDelegate
+
+@optional
+- (void) showMap;
+
+@end
+
+@class MoviePlayer;
+
+@interface Troubleshooting : UIViewController
+<UITableViewDelegate,
+UITableViewDataSource,
+ProductManualDelegate,
+ChatterDelegate,
+UIWebViewDelegate,
+UISearchBarDelegate>
+{
+    AppDelegate * appDelegate;
+    id <TroubleshootingDelegate> delegate;
+    
+    iOSInterfaceObject * iOSObject;
+    NSString * productName, * productId;
+    
+    ZipArchive * zip;
+
+    IBOutlet UITableView * tableView;
+    NSMutableArray * array;
+    
+    BOOL willGoToMap;
+    
+    IBOutlet UISearchBar * mSearchBar;
+    IBOutlet UIActivityIndicatorView * activity;
+    
+    int count;
+
+    MoviePlayer * moviePlayer;
+    
+    IBOutlet UILabel * subject;
+    IBOutlet UIWebView * webView;
+    
+    NSString * folderNameToCreate;
+    
+    IBOutlet UIButton * back, * forward;
+    
+    NSString * fileId, * fileName;
+    
+    int referenceCount;
+    
+    NSString * lastClickedFile;
+    NSUInteger lastClickedIndex;
+    
+    BOOL isLoaded;
+    
+    BOOL downloadInProgress;
+    
+    NSURLConnection * connection;
+    
+    //Radha 22nd April 2011
+    //For Localization
+    IBOutlet UIButton * backButton;
+    IBOutlet UINavigationItem * navBar;
+    
+    IBOutlet UINavigationBar * navigationBar;
+    
+    NSString * serviceMax, * alert_ok, * noMatch;
+    
+    BOOL didGetProductName;
+
+    BOOL didRunOperation;
+    
+    NSInteger _index;
+    
+    //shrinivas
+    BOOL isSessionInvalid;
+    
+    /*Accessibility changes*/
+    
+    IBOutlet UIImageView *servimaxLogo;
+    IBOutlet UIButton *productManual;
+    IBOutlet UIButton *chatter;
+    IBOutlet UIButton *helpButton;
+    IBOutlet UIButton *webviewBack;
+    IBOutlet UIButton *webviewForward;
+}
+
+
+@property (nonatomic, assign) id <TroubleshootingDelegate> delegate;
+@property (nonatomic, retain) NSString * productName;
+@property (nonatomic, retain) NSString * productId;
+@property BOOL isSessionInvalid;
+@property (nonatomic,retain) IBOutlet UINavigationBar * navigationBar;
+
+//- (void) showResults;//  Unused Methods
+
+//Radha 20th August 2011
+- (void) getProductNameForProductID:(NSString *)productTd;
+
+- (IBAction) Help;
+
+- (IBAction) Done;
+- (IBAction) GoToMap;
+
+- (IBAction) showProductManual;
+- (IBAction) showChatter;
+
+- (BOOL) unzipAndViewFile:(NSString *)_file;
+- (NSString *) showTroubleshootingForIndex:(NSUInteger)index;
+
+//- (void) showFirstTroubleshooting;//  Unused Methods
+
+- (IBAction) goPrev;
+- (IBAction) goNext;
+
+- (IBAction) launchSmartVan;
+
+
+@end
