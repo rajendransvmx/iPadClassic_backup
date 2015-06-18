bfc9196b8e3d412ce9cc962f74ff09cebe510fa8
niraj.kumar550612 <niraj.kumar550612@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-06-18 13:06:14 +0000

    Defect  018458: Checking product entry in InstallProduct table, If not there then doing null check

--- ServiceMaxiPad/Classes/Service/DAOService/CalenderEventObjectService.m
+++ ServiceMaxiPad/Classes/Service/DAOService/CalenderEventObjectService.m
@@ -16,6 +16,7 @@
 #import "TransactionObjectModel.h"
 #import "SFObjectDAO.h"
 #import "TransactionObjectDAO.h"
+#import "StringUtil.h"
 
 #import "DBRequestUpdate.h"
 
@@ -224,29 +225,43 @@
 {
     NSMutableArray * detailsArray = [[NSMutableArray alloc] initWithCapacity:0];
     @autoreleasepool {
-    DatabaseQueue *queue = [[DatabaseManager sharedInstance] databaseQueue];
-    
-    [queue inTransaction:^(SMDatabase *db, BOOL *rollback) {
-        NSString * query = [selectQuery query];
         
-        SQLResultSet * resultSet = [db executeQuery:query];
-        
-        while ([resultSet next]) {
-            NSDictionary * dict = [resultSet resultDictionary];
-            //Method in model to set the values
+            DatabaseQueue *queue = [[DatabaseManager sharedInstance] databaseQueue];
             
-            /* checkig value in product table, if SVMX_Product_c is there */
-            if (dict && ![dict isKindOfClass:[NSNull class]]){
-                if ([[dict allKeys] count]) {
-                    NSString *key =[[dict allKeys] objectAtIndex:0];
-                    if ([[dict valueForKey:key] length]) {
-                        [detailsArray addObject:[dict valueForKey:key]];
+            [queue inTransaction:^(SMDatabase *db, BOOL *rollback) {
+                NSString * query = [selectQuery query];
+                
+                /*
+                 Expcted query result is list of Product-id from Installed Product table.
+                 */
+                
+                SQLResultSet * resultSet = [db executeQuery:query];
+                
+                /*
+                 Iterate through the result set (in SVMXC__Product__c column) for getting product id
+                 */
+                
+                while ([resultSet next]) {
+                    
+                    NSDictionary * dict = [resultSet resultDictionary];
+                    
+                    
+                    if (   dict
+                        && (![dict isKindOfClass:[NSNull class]]))
+                       {
+                        if ([[dict allKeys] count] > 0) {
+                            
+                            NSString *key = [[dict allKeys] objectAtIndex:0];
+                            
+                            if (![StringUtil isStringEmpty:[dict valueForKey:key]])
+                            {
+                                [detailsArray addObject:[dict valueForKey:key]];
+                            }
+                        }
                     }
                 }
-            }
-        }
-        [resultSet close];
-    }];
+                [resultSet close];
+            }];
     }
     return detailsArray;
 }
