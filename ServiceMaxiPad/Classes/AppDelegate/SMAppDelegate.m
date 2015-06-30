af8c6275b6c46a9effb9ff46a89adec14d5593fe
anoopsaai.ramani <anoopsaai.ramani@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-06-30 16:47:35 +0000

    scheme URL changed to actual schema

--- ServiceMaxiPad/Classes/AppDelegate/SMAppDelegate.m
+++ ServiceMaxiPad/Classes/AppDelegate/SMAppDelegate.m
@@ -210,7 +210,7 @@ forLocalNotification:(UILocalNotification *)notification
 
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
 {
-    if ([[url scheme] isEqualToString:@"svmxmobilepulsesum15://"])
+    if ([[url scheme] isEqualToString:@"svmxmobilepulsesum15"])
     {
         NSDictionary *queryStringDictionary = [PushNotificationUtility getDictionaryFromSharedURL:url];
      
