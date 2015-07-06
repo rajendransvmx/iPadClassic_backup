c7de8d6f2891decf86b799f7e069b735c80dac09
niraj.kumar550612 <niraj.kumar550612@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-07-06 14:21:29 +0000

    Defect : 018834
    Changing the way we was saving record in table. Taking value from DB, replacing with updated value and committing the change.

--- ServiceMaxiPad/Classes/Parser/CustomWebServiceParser.m
+++ ServiceMaxiPad/Classes/Parser/CustomWebServiceParser.m
@@ -16,6 +16,8 @@
 #import "FactoryDAO.h"
 #import "DateUtil.h"
 #import "Utility.h"
+#import "DBRequestUpdate.h"
+#import "TransactionObjectService.h"
 
 @interface CustomWebServiceParser ()
 
@@ -30,42 +32,77 @@
                                      responseData:(id)responseData {
     
     @synchronized([self class]) {
-        if ([responseData isKindOfClass:[NSDictionary class]]) {
+        
+        if ([responseData isKindOfClass:[NSDictionary class]])
+        {
             self.helper = [[TXFetchHelper alloc] initWithCheckForDuplicateRecords:YES];
             NSDictionary *response = [NSDictionary dictionaryWithDictionary:responseData];
             NSArray *valueMap = [response objectForKey:kSVMXSVMXMap];
+            
             if ([valueMap isKindOfClass:[NSArray class]])
             {
-                
                 for (NSDictionary *recordDict in valueMap)
                 {
-                    NSMutableArray *objectrecords = [[NSMutableArray alloc] initWithCapacity:0];
+                    NSMutableDictionary *objectrecords = [[NSMutableDictionary alloc] initWithCapacity:0];
                     NSString *objectName = nil;
                     objectName = [recordDict objectForKey:kSVMXKey];
                     id str = [recordDict objectForKey:kSVMXValue];
                     NSMutableArray *dataArray = [Utility objectFromJsonString:str];
+                   
                     if ([dataArray isKindOfClass:[NSArray class]])
                     {
                         for (NSDictionary *dict in dataArray)
                         {
                             TransactionObjectModel *model = [[TransactionObjectModel alloc] initWithObjectApiName:objectName];
                             [model setFieldValueDictionaryForFields:dict];
-                            [objectrecords addObject:model];
+                            NSString *recordSFID = [dict objectForKey:kId];
+                            if (![StringUtil isStringEmpty:recordSFID])
+                                [objectrecords setObject:model forKey:recordSFID];
                         }
                     }
-                    else{
-                        //Here its structure problem, Its not a array
-                    }
-                    [self.helper insertObjects:objectrecords withObjectName:objectName];
+                    [self updateOrInsertTransactionObjectArray:objectrecords sfIdArray:[objectrecords allKeys] objectName:objectName];
+
                 }
-            }
-            else
-            {
-                //Here its structure problem, Its not a array
             }
         }
     }
     return nil;
+}
+
+- (void)updateOrInsertTransactionObjectArray:(NSMutableDictionary *)objectrecords sfIdArray:(NSArray*)sfidArray objectName:(NSString *)objectName
+{
+    NSArray *actualRecordsArray = [self getRecordsArrayForObjectName:objectName andSFIDArray:sfidArray];
+    NSMutableArray *updatedModelArray =[[NSMutableArray alloc] initWithCapacity:0];
+    
+    for (TransactionObjectModel *model in actualRecordsArray)
+    {
+        NSMutableDictionary *actualModelDict = [model getFieldValueMutableDictionary];
+        NSString *recordSFID = [actualModelDict objectForKey:kId];
+        TransactionObjectModel *toBeUpdatedModel = [objectrecords objectForKey:recordSFID];
+        NSMutableDictionary *toBeUpdatedDict = [toBeUpdatedModel getFieldValueMutableDictionary];
+        NSArray *toBeUpdatedAllKeys = [toBeUpdatedDict allKeys];
+        
+        for (NSString *keyString in toBeUpdatedAllKeys)
+        {
+            NSString *valueToBeUpdated = [toBeUpdatedDict valueForKey:keyString];
+            if (valueToBeUpdated != nil && [StringUtil isStringNotNULL:valueToBeUpdated] && [actualModelDict valueForKey:keyString])
+            {
+                [actualModelDict setValue:valueToBeUpdated forKey:keyString];
+            }
+        }
+        [model setFieldValueDictionaryForFields:actualModelDict];
+        [updatedModelArray addObject:model];
+    }
+    [self.helper insertObjects:updatedModelArray withObjectName:objectName];
+    
+}
+
+- (NSArray*)getRecordsArrayForObjectName:(NSString*)objName andSFIDArray:(NSArray*)array
+{
+    id <TransactionObjectDAO> transactionService = [FactoryDAO serviceByServiceType:ServiceTypeTransactionObject];
+    DBCriteria *criteria = [[DBCriteria alloc] initWithFieldName:kId operatorType:SQLOperatorIn andFieldValues:array];
+    NSArray *dataArray = [transactionService fetchDataForObject:objName fields:nil expression:nil criteria:@[criteria]];
+    return dataArray;
 }
 
 @end
