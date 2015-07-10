ea7637b7ce28eec0c3aaf8bbd72df2f5eb005f30
anoopsaai.ramani <anoopsaai.ramani@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-07-10 14:11:38 +0000

    013833 iPad Menu not rendered per specification

--- ServiceMaxiPad/Classes/TabBarComponents/CustomTabBar/CustomTabBar.m
+++ ServiceMaxiPad/Classes/TabBarComponents/CustomTabBar/CustomTabBar.m
@@ -1,6 +1,6 @@
 //
 //  CustomTabBar.m
-//  
+//
 //
 //
 
@@ -46,13 +46,11 @@ typedef enum {
 -(void)viewDidLoad
 {
     [super viewDidLoad];
-	
-
-	[self hideTabBar];
     
-    //self.view.backgroundColor = [UIColor colorWithHexString:@"f0f4c3"];
+    
+    [self hideTabBar];
     self.view.backgroundColor = [UIColor clearColor];
-
+    
     
     //HS
     //Add only 1st tab item not all
@@ -82,29 +80,22 @@ typedef enum {
     btn1.layer.shadowOffset = CGSizeMake(4.0f,4.5f);
     btn1.layer.shadowRadius = 1.5f;
     [self addBadgeToHomeButton];
-
+    
     CalendarHomeViewController *calendarVCDefault = [ViewControllerFactory createViewControllerByContext:ViewControllerCalendar];
     
     CalendarHomeViewController *calendarVC = [ViewControllerFactory createViewControllerByContext:ViewControllerCalendar];
-
+    
     [(SMXCalendarViewController *)calendarVCDefault removeAllEvents];
     
     SFMSearchViewController *exploreVC = [ViewControllerFactory createViewControllerByContext:ViewControllerExplore];
     
-//    ExploreHomeViewController *exploreVC = [ViewControllerFactory createViewControllerByContext:ViewControllerExplore];
-    
     TaskHomeViewController *taskVC = [ViewControllerFactory createViewControllerByContext:ViewControllerTasks];
-   //TroubleshootingViewController *taskVC = [ViewControllerFactory createViewControllerByContext:ViewControllerTroubleshooting];
-
     
     NewItemViewController *newItemVC = [ViewControllerFactory createViewControllerByContext:ViewControllerNewItem];
-    //    NewlyCreatedHomeController *newlyCreatedVC = [ViewControllerFactory createViewControllerByContext:ViewControllerNewlyCreated];
+
     RecentHomeViewController *recentHomeViewController = [ViewControllerFactory createViewControllerByContext:ViewControllerRecents];
-    // TroubleshootingHomeViewController *trobleshootingVC = [ViewControllerFactory createViewControllerByContext:ViewControllerTroubleshooting];
+
     ToolsHomeController *toolsCV = [ViewControllerFactory createViewControllerByContext:ViewControllerTools];
-    
-    
-    
     
     
     UINavigationController *navController = [[UINavigationController alloc]initWithRootViewController:calendarVCDefault];
@@ -119,11 +110,8 @@ typedef enum {
     UINavigationController *navController4 = [[UINavigationController alloc]initWithRootViewController:newItemVC];
     navController4.navigationBar.translucent = NO;
     UINavigationController *navController5 = [[UINavigationController alloc]initWithRootViewController:recentHomeViewController];
-    //UINavigationController *navController6 = [[UINavigationController alloc]initWithRootViewController:trobleshootingVC];
     UINavigationController *navController6 = [[UINavigationController alloc]initWithRootViewController:toolsCV];
     navController6.navigationBar.translucent = NO;
-    
-    //navController7.navigationBar.tintColor = [UIColor orangeColor];
     
     NSArray *tabItems = [[NSArray alloc]initWithObjects:navController,navController1,navController2,navController3,navController4,navController5,navController6, nil];
     
@@ -131,12 +119,12 @@ typedef enum {
     
     /* HS 3 Dec */
     // this below code is to show the tab bar for first launch and to set Calendar as first view.
-     [self addCustomElements];
+    [self addCustomElements];
     [self setSelectedIndex:1];
     _isTabClicked = YES;
     [self showNewTabBar];
     /* HS 3 Dc Ends here */
-
+    
     //[self.tabBar setSelectedItem:[self.tabBar.items objectAtIndex:1]];
     
 }
@@ -173,7 +161,7 @@ typedef enum {
     if (shouldAdd) {
         btn1.layer.shadowOpacity=0.50f;
         btn1.layer.shadowColor=[UIColor grayColor].CGColor;
-
+        
     } else {
         
         btn1.layer.shadowOpacity=0.0f;
@@ -184,15 +172,15 @@ typedef enum {
 
 - (void)hideTabBar
 {
-	for(UIView *view in self.view.subviews)
-	{
-		if([view isKindOfClass:[UITabBar class]])
-		{
-			view.hidden = YES;
-			break;
-		}
-	}
- }
+    for(UIView *view in self.view.subviews)
+    {
+        if([view isKindOfClass:[UITabBar class]])
+        {
+            view.hidden = YES;
+            break;
+        }
+    }
+}
 
 
 -(BOOL)shouldAutorotate
@@ -205,7 +193,7 @@ typedef enum {
     return UIInterfaceOrientationMaskAll;
 }
 
-- (void)hideNewTabBar 
+- (void)hideNewTabBar
 {
     
     // set up an animation for the transition between the views
@@ -214,14 +202,14 @@ typedef enum {
     [animation setType:kCATransitionPush];
     [animation setSubtype:kCATransitionFromRight];
     [animation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
-
+    
     [[tabBarView layer] addAnimation:animation forKey:@"SwitchToView1"];
     //Animation done
     
     tabBarView.hidden = 1;
     self.transparentView.hidden = YES;
     //self.btn1.hidden = 0;
-
+    
 }
 
 - (void)showNewTabBar
@@ -239,7 +227,7 @@ typedef enum {
         [self addShadowToMenuButton:NO];
         _isTabClicked = YES;
     }
-  
+    
 }
 
 -(void)addCustomElements
@@ -264,7 +252,7 @@ typedef enum {
     self.transparentView.translatesAutoresizingMaskIntoConstraints = YES;
     self.transparentView.backgroundColor = [UIColor clearColor];
     self.transparentView.alpha = 1;
-
+    
     self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)];
     [self.transparentView addGestureRecognizer:self.tapGestureRecognizer];
     [self.view addSubview:self.transparentView];
@@ -272,7 +260,7 @@ typedef enum {
      * End
      */
     
-	// Now we repeat the process for the other buttons
+    // Now we repeat the process for the other buttons
     CGRect theFrame;
     theFrame = btn1.frame;
     theFrame.origin.x = -2;
@@ -297,98 +285,98 @@ typedef enum {
     [self.view addSubview:tabBarView];
     
     CGFloat spaceMargin = (self.view.bounds.size.width - btn1.frame.origin.x + btn1.frame.size.width)/7 - 108; //As per spec(70 from left , for 1st button - 92, rest btn margin -131
-
-
+    
+    
     theFrame = btn1.frame;
     theFrame.origin.x = theFrame.origin.x + theFrame.size.width + spaceMargin+5  ;
     //theFrame.origin.x = theFrame.origin.x + 150 ;
-    theFrame.origin.y = 0;
-    theFrame.size.width = 110;
-    theFrame.size.height = 50;
+    theFrame.origin.y = 0.0f;
+    theFrame.size.width = 110.0f;
+    theFrame.size.height = 55.0f;
     
-
+    
     if (!self.btn2) {
         self.btn2 = [UIBuilder getTabBarButton:theFrame withImage:@"Calendar" withSelectedImage:@"Calendar" withTitle:[[TagManager sharedInstance]tagByName:kTagHomeCalendar]];
-        btn2.imageEdgeInsets = UIEdgeInsetsMake(6, 100/2-25/2, 22, 0);
+        btn2.imageEdgeInsets = UIEdgeInsetsMake(5, 100/2-25/2, 22, 0);
         btn2.titleEdgeInsets = UIEdgeInsetsMake(25, -25, 0, 0);
         [btn2 setTag:TabBarItemIDCalendar];
         [btn2 setSelected:YES];
         btn2.backgroundColor = [UIColor clearColor];
-        [btn2 setImage:[UIImage imageNamed:@"NavBarCal.png"] forState:UIControlStateNormal];
-        [btn2 setImage:[UIImage imageNamed:@"SCalendar.png"] forState:UIControlStateSelected];
+        [btn2 setImage:[UIImage imageNamed:@"NavBarCal"] forState:UIControlStateNormal];
+        [btn2 setImage:[UIImage imageNamed:@"SCalendar"] forState:UIControlStateSelected];
     }
     
-    theFrame.origin.x = theFrame.origin.x + theFrame.size.width + spaceMargin +5 ;
+    theFrame.origin.x = theFrame.origin.x + theFrame.size.width + spaceMargin + 5;
     
     if (!self.btn3) {
         self.btn3 = [UIBuilder getTabBarButton:theFrame withImage:@"Explore" withSelectedImage:@"Explore" withTitle:[[TagManager sharedInstance]tagByName:kTagExplore]];
         
-        btn3.imageEdgeInsets = UIEdgeInsetsMake(6, 100/2-25/2, 22, 0);
+        btn3.imageEdgeInsets = UIEdgeInsetsMake(5, 100/2-25/2, 22, 0);
         btn3.titleEdgeInsets = UIEdgeInsetsMake(25, -25, 0, 0);
         
         [btn3 setTag:TabBarItemIDExplore];
         btn3.backgroundColor = [UIColor clearColor];
         
-        [btn3 setImage:[UIImage imageNamed:@"NavBarExplore.png"] forState:UIControlStateNormal];
-        [btn3 setImage:[UIImage imageNamed:@"SExplore.png"] forState:UIControlStateSelected];
-
+        [btn3 setImage:[UIImage imageNamed:@"NavBarExplore"] forState:UIControlStateNormal];
+        [btn3 setImage:[UIImage imageNamed:@"SExplore"] forState:UIControlStateSelected];
+        
     }
-
+    
     theFrame.origin.x = theFrame.origin.x + theFrame.size.width + spaceMargin +5 ;
     if (!self.btn4) {
         self.btn4 = [UIBuilder getTabBarButton:theFrame withImage:@"Task" withSelectedImage:@"Task" withTitle:[[TagManager sharedInstance]tagByName:kTagHomeTask]];
         
         
-        btn4.imageEdgeInsets = UIEdgeInsetsMake(6, 100/2-25/2, 22, 0);
+        btn4.imageEdgeInsets = UIEdgeInsetsMake(5, 100/2-25/2, 22, 0);
         btn4.titleEdgeInsets = UIEdgeInsetsMake(25, -25, 0, 0);
         
         [btn4 setTag:TabBarItemIDTask];
         btn4.backgroundColor = [UIColor clearColor];
         
-        [btn4 setImage:[UIImage imageNamed:@"NavBarTasks.png"] forState:UIControlStateNormal];
-        [btn4 setImage:[UIImage imageNamed:@"STasks.png"] forState:UIControlStateSelected];
-
+        [btn4 setImage:[UIImage imageNamed:@"NavBarTasks"] forState:UIControlStateNormal];
+        [btn4 setImage:[UIImage imageNamed:@"STasks"] forState:UIControlStateSelected];
+        
     }
     
     theFrame.origin.x = theFrame.origin.x + theFrame.size.width + spaceMargin +5 ;
     if (!self.btn5) {
         self.btn5 = [UIBuilder getTabBarButton:theFrame withImage:@"NewItem" withSelectedImage:@"NewItem" withTitle:[[TagManager sharedInstance]tagByName:kTagNewItem]];
-        btn5.imageEdgeInsets = UIEdgeInsetsMake(6, 100/2-25/2, 22, 0);
+        btn5.imageEdgeInsets = UIEdgeInsetsMake(5, 100/2-25/2, 22, 0);
         btn5.titleEdgeInsets = UIEdgeInsetsMake(25, -25, 0, 0);
         
         [btn5 setTag:TabBarItemIDNewItem];
         btn5.backgroundColor = [UIColor clearColor];
-        [btn5 setImage:[UIImage imageNamed:@"SNewItem.png"] forState:UIControlStateSelected];
-        [btn5 setImage:[UIImage imageNamed:@"NavBarNew.png"] forState:UIControlStateNormal];
-
-
+        [btn5 setImage:[UIImage imageNamed:@"SNewItem"] forState:UIControlStateSelected];
+        [btn5 setImage:[UIImage imageNamed:@"NavBarNew"] forState:UIControlStateNormal];
+        
+        
         
         
         
     }
-
+    
     theFrame.origin.x = theFrame.origin.x + theFrame.size.width + spaceMargin +5 ;
     if (!self.btn6) {
         self.btn6 = [UIBuilder getTabBarButton:theFrame withImage:@"Recents" withSelectedImage:@"Recents" withTitle:[[TagManager sharedInstance]tagByName:kTagRecentlyCreated]];
-        btn6.imageEdgeInsets = UIEdgeInsetsMake(6, 100/2-25/2, 22, 0);
+        btn6.imageEdgeInsets = UIEdgeInsetsMake(5, 100/2-25/2, 22, 0);
         btn6.titleEdgeInsets = UIEdgeInsetsMake(25, -25, 0, 0);
         [btn6 setTag:TabBarItemIDRecents];
         btn6.backgroundColor = [UIColor clearColor];
-        [btn6 setImage:[UIImage imageNamed:@"SRecents.png"] forState:UIControlStateSelected];
-        [btn6 setImage:[UIImage imageNamed:@"NavBarRecents.png"] forState:UIControlStateNormal];
-
-
+        [btn6 setImage:[UIImage imageNamed:@"SRecents"] forState:UIControlStateSelected];
+        [btn6 setImage:[UIImage imageNamed:@"NavBarRecents"] forState:UIControlStateNormal];
+        
+        
     }
     
     theFrame.origin.x = theFrame.origin.x + theFrame.size.width + spaceMargin +5 ;
     if (!self.btn7) {
         self.btn7 = [UIBuilder getTabBarButton:theFrame withImage:@"Tools" withSelectedImage:@"Tools" withTitle:[[TagManager sharedInstance]tagByName:kTagTools]  withBadge:YES];
-        btn7.imageEdgeInsets = UIEdgeInsetsMake(6, 100/2-25/2, 22, 0);
+        btn7.imageEdgeInsets = UIEdgeInsetsMake(5, 100/2-25/2, 22, 0);
         btn7.titleEdgeInsets = UIEdgeInsetsMake(25, -25, 0, 0);
         [btn7 setTag:TabBarItemIDTools];
         btn7.backgroundColor = [UIColor clearColor];
-        [btn7 setImage:[UIImage imageNamed:@"STools.png"] forState:UIControlStateSelected];
-        [btn7 setImage:[UIImage imageNamed:@"NavBarTools.png"] forState:UIControlStateNormal];
+        [btn7 setImage:[UIImage imageNamed:@"STools"] forState:UIControlStateSelected];
+        [btn7 setImage:[UIImage imageNamed:@"NavBarTools"] forState:UIControlStateNormal];
         
         [self updateBadge];
     }
@@ -397,24 +385,24 @@ typedef enum {
     
     
     
-	// Setup event handlers so that the buttonClicked method will respond to the touch up inside event.
-	//[btn1 addTarget:self action:@selector(btnPressShow:) forControlEvents:UIControlEventTouchUpInside];
-	[btn2 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-	[btn3 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-	[btn4 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    // Setup event handlers so that the buttonClicked method will respond to the touch up inside event.
+    //[btn1 addTarget:self action:@selector(btnPressShow:) forControlEvents:UIControlEventTouchUpInside];
+    [btn2 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [btn3 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [btn4 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
     [btn5 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-	[btn6 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-	[btn7 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-	//[btn8 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [btn6 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [btn7 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    //[btn8 addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
     
     
     //HS new ends
     
     
     
-	
-	// Add my new buttons to the view
-	//[self.view addSubview:btn1];
+    
+    // Add my new buttons to the view
+    //[self.view addSubview:btn1];
     
     // set up an animation for the transition between the views
     CATransition *animation = [CATransition animation];
@@ -426,13 +414,13 @@ typedef enum {
     
     //Animation done
     
-	[tabBarView addSubview:btn2];
-	[tabBarView addSubview:btn3];
-	[tabBarView addSubview:btn4];
+    [tabBarView addSubview:btn2];
+    [tabBarView addSubview:btn3];
+    [tabBarView addSubview:btn4];
     [tabBarView addSubview:btn5];
-	[tabBarView addSubview:btn6];
-	[tabBarView addSubview:btn7];
-	//[tabBarView addSubview:btn8];
+    [tabBarView addSubview:btn6];
+    [tabBarView addSubview:btn7];
+    //[tabBarView addSubview:btn8];
     if (btn1)
     {
         [btn1 removeFromSuperview];
@@ -450,8 +438,8 @@ typedef enum {
 
 - (void)buttonClicked:(id)sender
 {
-	NSInteger tagNum = [sender tag];
-	[self selectTab:tagNum];
+    NSInteger tagNum = [sender tag];
+    [self selectTab:tagNum];
     [self showNewTabBar];
 }
 
@@ -466,9 +454,9 @@ typedef enum {
         [srchvc reloadDataOnTabButtonClick];
     }
     
-	self.selectedIndex = tabID;
-	
- }
+    self.selectedIndex = tabID;
+    
+}
 
 - (void)updateButtonStateForSelectedTabId:(NSInteger)tabID {
     
@@ -487,9 +475,9 @@ typedef enum {
 - (void)updateBadge
 {
     NSUInteger count = [[ResolveConflictsHelper getConflictsRecords] count];
-
+    
     SXLogWarning(@"Conflicts count %lu",(unsigned long)count);
-
+    
     CustomBadge *toolsBadge = (CustomBadge*)[self.btn7 viewWithTag:BADGE_TAG];
     CustomBadge *homeBadge = (CustomBadge*)[self.btn1 viewWithTag:BADGE_TAG];
     
@@ -512,7 +500,7 @@ typedef enum {
     if(homeBadge != nil) {
         [homeBadge autoBadgeSizeWithString:counterStr];
     }
-
+    
     
 }
 
