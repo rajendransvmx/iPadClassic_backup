0416dc4eb697cf7418bc68c517f3071d67c449a9
niraj.kumar550612 <niraj.kumar550612@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-08-27 07:02:50 +0000

    Price book changes, Adding new classes for query

---
+++ ServiceMaxiPad/Classes/Service/ServiceLayer/SVMXGetPriceList.h
@@ -0,0 +1,16 @@
+//
+//  SVMXGetPriceList.h
+//  ServiceMaxiPad
+//
+//  Created by Apple on 26/08/15.
+//  Copyright (c) 2015 ServiceMax Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CommonServiceDAO.h"
+
+@interface SVMXGetPriceList : NSObject
+
+-(NSArray *)getDistinctProductIds;
+-(NSArray *)getDistinctDataCustomPriceBookIds;
+@end
---
+++ ServiceMaxiPad/Classes/Service/ServiceLayer/SVMXGetPriceList.m
@@ -0,0 +1,61 @@
+//
+//  SVMXGetPriceList.m
+//  ServiceMaxiPad
+//
+//  Created by Apple on 26/08/15.
+//  Copyright (c) 2015 ServiceMax Inc. All rights reserved.
+//
+
+#import "SVMXGetPriceList.h"
+#import "DBRequestSelect.h"
+#import "DatabaseQueue.h"
+#import "SQLResultSet.h"
+#import "DatabaseManager.h"
+#import "SyncRecordHeapModel.h"
+#import "SVMXSystemConstant.h"
+
+@implementation SVMXGetPriceList
+
+-(NSArray *)getDistinctProductIds
+{
+//    DBRequestSelect * requestSelect = [[DBRequestSelect alloc] initWithTableName:kDataPurgePriceBook andFieldNames:[NSArray arrayWithObjects:kId, nil] whereCriteria:nil];
+//    
+//    [requestSelect setDistinctRowsOnly];
+//    
+//    return [self getRecordsFromQuery:[requestSelect query]];
+//    
+//    return nil;
+    return @[];
+}
+-(NSArray *)getDistinctDataCustomPriceBookIds
+{
+//    DBRequestSelect * requestSelect = [[DBRequestSelect alloc] initWithTableName:kDataPurgeCustomPriceBook andFieldNames:[NSArray arrayWithObjects:kId, nil] whereCriteria:nil];
+//    
+//    [requestSelect setDistinctRowsOnly];
+//    
+//    return [self getRecordsFromQuery:[requestSelect query]];
+//    
+//    return nil;
+    return @[];
+}
+
+- (NSArray*)getRecordsFromQuery:(NSString*)query
+{
+    NSMutableArray * records = [[NSMutableArray alloc] initWithCapacity:0];
+    @autoreleasepool {
+        DatabaseQueue *queue = [[DatabaseManager sharedInstance] databaseQueue];
+        
+        [queue inTransaction:^(SMDatabase *db, BOOL *rollback) {
+            SQLResultSet * resultSet = [db executeQuery:query];
+            
+            while ([resultSet next]) {
+                SyncRecordHeapModel * model = [[SyncRecordHeapModel alloc] init];
+                [resultSet kvcMagic:model];
+                [records addObject:model];
+            }
+            [resultSet close];
+        }];
+    }
+    return records;
+}
+@end
