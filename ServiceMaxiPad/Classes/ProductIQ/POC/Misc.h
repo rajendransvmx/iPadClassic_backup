a389e1b4fe62f2b47b037cae31b8f6bf8aef6531
rahman.s <rahman.s@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-10-04 11:29:31 +0000

    Added native call support for: CheckConnectivity for ProductIQ.

---
+++ ServiceMaxiPad/Classes/ProductIQ/POC/Misc.h
@@ -0,0 +1,15 @@
+//
+//  Misc.h
+//  ServiceMaxiPad
+//
+//  Created by Rahman Sab C on 02/10/15.
+//  Copyright (c) 2015 ServiceMax Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Misc : NSObject
+
+- (void)checkNetworkReachability:(NSString*)params;
+
+@end
---
+++ ServiceMaxiPad/Classes/ProductIQ/POC/Misc.m
@@ -0,0 +1,62 @@
+//
+//  Misc.m
+//  ServiceMaxiPad
+//
+//  Created by Rahman Sab C on 02/10/15.
+//  Copyright (c) 2015 ServiceMax Inc. All rights reserved.
+//
+
+#import "Misc.h"
+#import "ProductIQPOCHomeViewController.h"
+#import "SNetworkReachabilityManager.h"
+
+@implementation Misc
+
+- (void)checkNetworkReachability:(NSString*)params {
+    
+    @autoreleasepool {
+        NSDictionary *requestParams = [self parse:params];
+        NSString *callback = requestParams[@"nativeCallbackHandler"];
+        NSString *requestId = requestParams[@"requestId"];
+        NSString *type = requestParams[@"type"];
+        NSString *methodName = requestParams[@"methodName"];
+        NSString *jsCallback = requestParams[@"jsCallback"];
+        
+        BOOL isNetworkReachable = [[SNetworkReachabilityManager sharedInstance] isNetworkReachable];
+        NSMutableDictionary *networkDictionary = [[NSMutableDictionary alloc] initWithCapacity:0];
+        [networkDictionary setObject:[NSNumber numberWithBool:isNetworkReachable] forKey:@"ResultAsString"];
+        [networkDictionary setObject:[NSNumber numberWithBool:isNetworkReachable] forKey:@"IsSuccessful"];
+        
+        
+        NSMutableDictionary *responseDictionary = [[NSMutableDictionary alloc] initWithCapacity:0];
+        [responseDictionary setObject:requestId forKey:@"requestId"];
+        [responseDictionary setObject:type forKey:@"type"];
+        [responseDictionary setObject:methodName forKey:@"methodName"];
+        [responseDictionary setObject:callback forKey:@"nativeCallbackHandler"];
+        [responseDictionary setObject:jsCallback forKey:@"jsCallback"];
+        [responseDictionary setObject:networkDictionary forKey:@"networkStatus"];
+        [self respondOnMethod:callback withParams:responseDictionary];
+
+    }
+    
+}
+
+-(NSDictionary *)parse:(NSString *) str {
+    NSError *error = nil;
+    NSDictionary *ret =
+    [NSJSONSerialization JSONObjectWithData:[str dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&error];
+    return ret;
+}
+
+-(void)respondOnMethod:(NSString *) methodName withParams:(NSDictionary *)params {
+    NSError *error = nil;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:params options:0 error:&error];
+    NSString *resp = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    UIWebView *browser = [[ProductIQPOCHomeViewController getInstance] getBrowser];
+    NSString *js = [NSString stringWithFormat:@"%@(%@)", methodName, resp];
+    NSLog(@"&&& %@", js);
+    [browser stringByEvaluatingJavaScriptFromString:js];
+}
+
+@end
