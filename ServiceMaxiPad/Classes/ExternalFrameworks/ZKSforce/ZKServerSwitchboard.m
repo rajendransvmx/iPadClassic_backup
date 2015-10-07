ea7d75853661bff8eb496e89fa98cf66e483a0a4
Himanshi.Sharma <Himanshi.Sharma@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-10-07 09:04:12 +0000

    Fixed issue : 023391 Changed BaseURL from "www.salesforce.com" to "https://login.salesforce.com" as per Salesforce confirmation under below statement

--- ServiceMaxiPad/Classes/ExternalFrameworks/ZKSforce/ZKServerSwitchboard.m
+++ ServiceMaxiPad/Classes/ExternalFrameworks/ZKSforce/ZKServerSwitchboard.m
@@ -190,7 +190,17 @@ static ZKServerSwitchboard * sharedSwitchboard =  nil;
         return customURL;
 
 	}
-    return @"https://www.salesforce.com";
+    /*
+     Fixed Issue :023391
+     HS 7Oct2016 Changed BaseURL from "www.salesforce.com" to "https://login.salesforce.com" as per Salesforce confirmation under below statement
+     As of January 1, 2016, the internal routing will be retired, and any API endpoints will need to be changed if from www.salesforce.com to https://login.salesforce.com.
+     */
+    //return @"https://www.salesforce.com";
+    return @"https://login.salesforce.com";
+
+
+
+    
 }
 
 #pragma mark Properties
