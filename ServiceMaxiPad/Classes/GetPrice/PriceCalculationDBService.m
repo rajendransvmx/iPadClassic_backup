3425fcc3bd91b7276c03b5c88fe6778bc5da1ff7
Himanshi.Sharma <Himanshi.Sharma@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-11-29 17:11:04 +0000

    Fixed Defect:25261

--- ServiceMaxiPad/Classes/GetPrice/PriceCalculationDBService.m
+++ ServiceMaxiPad/Classes/GetPrice/PriceCalculationDBService.m
@@ -623,20 +623,29 @@
             
             /*get the reference id from manifest */
             NSString *snippetId =  [self getCodeSnippetRefererenceForId:codeSnippetId];
-            NSDictionary *tempDictionary =  [self getCodeSnippetForNameOrId:snippetId];
-            if ([tempDictionary count] > 0) {
-                codeSnippetId = [tempDictionary objectForKey:@"Id"];
-                NSString *tempStr = [tempDictionary objectForKey:kCodeSnippetData];
-                if(![StringUtil isStringEmpty:codeSnippetId] && ![StringUtil isStringEmpty:tempStr] ) {
-                    [codeSnippetFinal appendFormat:@" %@",tempStr];
+            //Defect Fix:025261 , checking nil for snippetId, if there is no longer reference for snippet in manifest table
+            if (snippetId !=nil)
+            {
+                NSDictionary *tempDictionary =  [self getCodeSnippetForNameOrId:snippetId];
+                if ([tempDictionary count] > 0) {
+                    codeSnippetId = [tempDictionary objectForKey:@"Id"];//instead of codesnipper id we shd use only referernce id
+                    NSString *tempStr = [tempDictionary objectForKey:kCodeSnippetData];
+                    if(![StringUtil isStringEmpty:codeSnippetId] && ![StringUtil isStringEmpty:tempStr] ) {
+                        [codeSnippetFinal appendFormat:@" %@",tempStr];
+                    }
+                    else {
+                        codeSnippetId = nil;
+                    }
                 }
                 else {
                     codeSnippetId = nil;
                 }
             }
-            else {
+            else
+            {
                 codeSnippetId = nil;
             }
+          
             
         }
     }
