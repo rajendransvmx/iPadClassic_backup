d69d5201080c55f26067ffcdc1a327bd0e2368a6
rahman.s <rahman.s@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2015-12-11 03:43:59 +0000

    Fixed: Invoking tree view from WO expands tree upto component but displays top level IB in the middle pane if WO top level is not immediate parent of WO comp(025873).

--- ServiceMaxiPad/Classes/ProductIQ/ProductIQHomeViewController.m
+++ ServiceMaxiPad/Classes/ProductIQ/ProductIQHomeViewController.m
@@ -285,7 +285,7 @@ static  ProductIQHomeViewController *instance;
 }
 
 - (void)handleNativeCall:(NSString *)url {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_MSEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [bridge invoke: url];
     });
 }
