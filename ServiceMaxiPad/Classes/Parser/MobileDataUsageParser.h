2f7a56f8b13bc98d0b867928785571ac84a69897
Himanshi.Sharma <Himanshi.Sharma@5a4fd11e-6138-4d43-ace2-00eb939f7215>
2016-03-09 07:11:54 +0000

    Added Static libraries for MobileDataUsage Added Js files for MobileDataUSage Added new files for handling native calls for MobileDataUsage

---
+++ ServiceMaxiPad/Classes/Parser/MobileDataUsageParser.h
@@ -0,0 +1,13 @@
+//
+//  MobileDataUsageParser.h
+//  ServiceMaxiPad
+//
+//  Created by Himanshi Sharma on 02/03/16.
+//  Copyright © 2016 ServiceMax Inc. All rights reserved.
+//
+
+#import "WebServiceParser.h"
+
+@interface MobileDataUsageParser : WebServiceParser
+
+@end
