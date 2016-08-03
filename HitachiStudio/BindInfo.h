2d1ba2acb05e90f708f225da2444971d3ad223fd
Padmashree J <PadmashreeJ@users.noreply.github.com>
2016-08-03 06:57:18 +0000

    Committed latest verifaya lib files.

---
+++ HitachiStudio/BindInfo.h
@@ -0,0 +1,13 @@
+//
+//  BindInfo.h
+//  VerifayaStudio
+//
+//  Created by Manjukiran on 7/21/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BindInfo : NSObject
+-(void)printLibraryVersion;
+@end
