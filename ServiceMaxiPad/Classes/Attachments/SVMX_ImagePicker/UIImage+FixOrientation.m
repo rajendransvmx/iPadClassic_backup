3ff3a7577859c064068f1732c4f59d3aa8f04e49
Sruthi Ramakrishnan <sruthi.ramakrishnan@servicemax.com>
2016-11-18 16:10:12 +0530

    PCRD-78 Issue # 90 - Forensic Similarities to StackOverflow Article, "iphone - iOS UIImagePickerController result image orientation after upload" (CC-BY-SA 3.0)

--- ServiceMaxiPad/Classes/Attachments/SVMX_ImagePicker/UIImage+FixOrientation.m
+++ ServiceMaxiPad/Classes/Attachments/SVMX_ImagePicker/UIImage+FixOrientation.m
@@ -12,82 +12,64 @@
 
 - (UIImage *)fixImageOrientation {
     
+    UIImage *imageSelf = self;
+    
     // If the orientation is already correct, do nothing
-    if (self.imageOrientation == UIImageOrientationUp) {
-        return self;
+    if (imageSelf.imageOrientation == UIImageOrientationUp) {
+        return imageSelf;
     }
     
     // There are two steps to calculate the transformation to make the image upright - Rotate if the image is left or right or down, and then flip if image is mirrored.
     CGAffineTransform afflineTransform = CGAffineTransformIdentity;
     
-    switch (self.imageOrientation) {
-        case UIImageOrientationDown:
-        case UIImageOrientationDownMirrored:
-        {
-            afflineTransform = CGAffineTransformTranslate(afflineTransform, self.size.width, self.size.height);
-            afflineTransform = CGAffineTransformRotate(afflineTransform, M_PI);
-            break;
-        }
-            
-        case UIImageOrientationLeft:
-        case UIImageOrientationLeftMirrored:
-        {
-            afflineTransform = CGAffineTransformTranslate(afflineTransform, self.size.width, 0);
-            afflineTransform = CGAffineTransformRotate(afflineTransform, M_PI_2);
-            break;
-        }
-            
-        case UIImageOrientationRight:
-        case UIImageOrientationRightMirrored:
-        {
-            afflineTransform = CGAffineTransformTranslate(afflineTransform, 0, self.size.height);
-            afflineTransform = CGAffineTransformRotate(afflineTransform, -M_PI_2);
-            break;
-        }
-            
-        case UIImageOrientationUp:
-        case UIImageOrientationUpMirrored:
-            break;
+    //step 1
+    //check for down orientation
+    if (imageSelf.imageOrientation == UIImageOrientationDown || imageSelf.imageOrientation == UIImageOrientationDownMirrored) {
+        
+        afflineTransform = CGAffineTransformTranslate(afflineTransform, imageSelf.size.width, imageSelf.size.height);
+        afflineTransform = CGAffineTransformRotate(afflineTransform, M_PI);
     }
-    
-    switch (self.imageOrientation) {
-        case UIImageOrientationUpMirrored:
-        case UIImageOrientationDownMirrored:
-        {
-            afflineTransform = CGAffineTransformTranslate(afflineTransform, self.size.width, 0);
-            afflineTransform = CGAffineTransformScale(afflineTransform, -1, 1);
-            break;
-        }
-            
-        case UIImageOrientationLeftMirrored:
-        case UIImageOrientationRightMirrored:
-        {
-            afflineTransform = CGAffineTransformTranslate(afflineTransform, self.size.height, 0);
-            afflineTransform = CGAffineTransformScale(afflineTransform, -1, 1);
-            break;
-        }
-            
-        case UIImageOrientationUp:
-        case UIImageOrientationDown:
-        case UIImageOrientationLeft:
-        case UIImageOrientationRight:
-            break;
+    //check for left orientation
+    else if (imageSelf.imageOrientation == UIImageOrientationLeft || imageSelf.imageOrientation == UIImageOrientationLeftMirrored) {
+        
+        afflineTransform = CGAffineTransformTranslate(afflineTransform, imageSelf.size.width, 0);
+        afflineTransform = CGAffineTransformRotate(afflineTransform, M_PI_2);
+    }
+    //check for right orientation
+    else if (imageSelf.imageOrientation == UIImageOrientationRight || imageSelf.imageOrientation == UIImageOrientationRightMirrored) {
+
+        afflineTransform = CGAffineTransformTranslate(afflineTransform, 0, imageSelf.size.height);
+        afflineTransform = CGAffineTransformRotate(afflineTransform, -M_PI_2);
+    }
+
+    //step 2
+    //check for up or down orientation
+    if (imageSelf.imageOrientation == UIImageOrientationUpMirrored || imageSelf.imageOrientation == UIImageOrientationDownMirrored) {
+
+        afflineTransform = CGAffineTransformTranslate(afflineTransform, imageSelf.size.width, 0);
+        afflineTransform = CGAffineTransformScale(afflineTransform, -1, 1);
+    }
+    //check for left or right orientation
+    else if (imageSelf.imageOrientation == UIImageOrientationLeftMirrored || imageSelf.imageOrientation == UIImageOrientationRightMirrored) {
+
+        afflineTransform = CGAffineTransformTranslate(afflineTransform, imageSelf.size.height, 0);
+        afflineTransform = CGAffineTransformScale(afflineTransform, -1, 1);
     }
     
     // Draw underlying CGImage into a new context and then applying the transform calculated above.
-    CGContextRef context = CGBitmapContextCreate(NULL, self.size.width, self.size.height,
-                                                 CGImageGetBitsPerComponent(self.CGImage), 0,
-                                                 CGImageGetColorSpace(self.CGImage),
-                                                 CGImageGetBitmapInfo(self.CGImage));
+    CGContextRef context = CGBitmapContextCreate(NULL, imageSelf.size.width, imageSelf.size.height,
+                                                 CGImageGetBitsPerComponent(imageSelf.CGImage), 0,
+                                                 CGImageGetColorSpace(imageSelf.CGImage),
+                                                 CGImageGetBitmapInfo(imageSelf.CGImage));
     CGContextConcatCTM(context, afflineTransform);
     
-    if (self.imageOrientation == UIImageOrientationLeft || self.imageOrientation == UIImageOrientationLeftMirrored || self.imageOrientation == UIImageOrientationRight || self.imageOrientation == UIImageOrientationRightMirrored) {
+    if (imageSelf.imageOrientation == UIImageOrientationLeft || imageSelf.imageOrientation == UIImageOrientationLeftMirrored || imageSelf.imageOrientation == UIImageOrientationRight || imageSelf.imageOrientation == UIImageOrientationRightMirrored) {
         
-        CGContextDrawImage(context, CGRectMake(0,0,self.size.height,self.size.width), self.CGImage);
+        CGContextDrawImage(context, CGRectMake(0,0,imageSelf.size.height,imageSelf.size.width), imageSelf.CGImage);
     }
     else {
         
-        CGContextDrawImage(context, CGRectMake(0,0,self.size.width,self.size.height), self.CGImage);
+        CGContextDrawImage(context, CGRectMake(0,0,imageSelf.size.width,imageSelf.size.height), imageSelf.CGImage);
     }
     
     // Create new UIImage from the drawing context
