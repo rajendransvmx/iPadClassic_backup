77f3bfb72e66b786baf456b78cf2dedb6a3307c0
Padmashree <padmashree.j@servicemax.com>
2017-07-05 14:19:17 +0530

    IPAD-4595 :  fixed checkbox and date field related issues.

--- ServiceMaxiPad/Classes/SFMPage/SFMPageService/SFMPageEditManager.m
+++ ServiceMaxiPad/Classes/SFMPage/SFMPageService/SFMPageEditManager.m
@@ -2173,12 +2173,14 @@
                 
                 if([fieldType isEqualToString:kSfDTDate] || [fieldType isEqualToString:kSfDTDateTime])
                 {
-                    if ([self isItALiteral:finalValue]) {
-                        //defect#036563
-                        finalValue = [DateUtil evaluateDateLiteral:finalValue  dataType:fieldType];
-                    }
-                    if ([fieldType isEqualToString:kSfDTDate]) {
-                        finalValue = [self getDateForMapping:finalValue];
+                    if (![StringUtil isStringEmpty:finalValue]) { // IPAD-4595
+                        if ([self isItALiteral:finalValue]) {
+                            //defect#036563
+                            finalValue = [DateUtil evaluateDateLiteral:finalValue  dataType:fieldType];
+                        }
+                        if ([fieldType isEqualToString:kSfDTDate]) {
+                            finalValue = [self getDateForMapping:finalValue];
+                        }
                     }
                 }
                 else if ([StringUtil containsString:kLiteralCurrentRecord inString:finalValue])
@@ -2194,6 +2196,8 @@
                     {
                         valueOfLiteral = finalValue;
                     }
+                    
+                    finalValue = valueOfLiteral; // IPAD-4595
                 }
             }
             else if ([model.action isEqualToString:@"Increase"]){
